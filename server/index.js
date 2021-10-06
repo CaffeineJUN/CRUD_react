f2e9115d1e0c295403ca5a19a6e93bff862999d5
BitterCoffeeJun <gurwns232@naver.com>
2021-10-06 23:39:27 +0900

    - server getpost, createpost routes 작성

--- server/index.js
+++ server/index.js
@@ -2,8 +2,26 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import postRoutes from './routes/posts.js'
+
 const app = express()
+const CONNECTION_URL = 'mongodb+srv://BitterCoffee:hyeok0560@boilerplate.47hnt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
 
 app.use(express.json({limit: '30mb', extended: true}))
 app.use(express.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
+
+app.use('/posts', postRoutes)
+
+mongoose
+    .connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port: ${PORT}`)
+        })
+    })
+    .catch(error => console.log(error.message))
