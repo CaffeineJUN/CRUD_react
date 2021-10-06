1d015b894d2e3815662174bb31cef6e5793260ac
BitterCoffeeJun <gurwns232@naver.com>
2021-10-06 22:30:23 +0900

    MERN CRUD project base setting

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+const app = express()
+
+app.use(express.json({limit: '30mb', extended: true}))
+app.use(express.urlencoded({limit: '30mb', extended: true}))
+app.use(cors())
