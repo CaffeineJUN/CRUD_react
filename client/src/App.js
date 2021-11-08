3d9dd125d9b676a5f50ddc35cf04e58360346419
BitterCoffeeJun <gurwns232@naver.com>
2021-11-08 22:09:10 +0900

    ,,

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+
+import memories from './images/coffee_word_inscription_156916_1920x1080 - 복사본.jpg'
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+
 function App() {
     return (
-        <div>
-            <h1>hello</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">
+                    Memories
+                </Typography>
+                <img src={memories} alt="memories" height="60" width="80" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
